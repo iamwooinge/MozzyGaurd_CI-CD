8027cad7c3ae3798c4ecf1278b5df1b0cafa4e7f
kimgywls <wh980213@naver.com>
2024-07-21 23:03:34 +0900

    db config
    root

---
+++ mysql_config.py
@@ -0,0 +1,10 @@
+# mysql_config.py
+import pymysql.cursors
+
+mysql_db = {
+    "user": "root",
+    "password": "abc1234!!",
+    "host": "localhost",
+    "port": 3306,
+    "database": "locations",
+}
