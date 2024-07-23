d99457e55ee2166a328e58bf180eaf8398178fa7
kimgywls <wh980213@naver.com>
2024-07-24 03:01:20 +0900

    update all

--- mysql_config.py
+++ mysql_config.py
@@ -2,8 +2,8 @@
 import pymysql.cursors
 
 mysql_db = {
-    "user": "khj",
-    "password": "100609",
+    "user": "root",
+    "password": "abc1234!!",
     "host": "localhost",
     "port": 3306,
     "database": "mozzy_guard_db",
