dc2f376035e5e542c4c026998db787e05ed966ce
kimgywls <wh980213@naver.com>
2024-07-24 11:12:23 +0900

    subscribe modify

--- mysql_config.py
+++ mysql_config.py
@@ -1,5 +1,7 @@
 # mysql_config.py
 import pymysql.cursors
+import mysql.connector
+from mysql.connector import Error
 
 mysql_db = {
     "user": "khj",
@@ -8,3 +10,18 @@ mysql_db = {
     "port": 3306,
     "database": "mozzy_guard_db",
 }
+
+def get_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host=mysql_db['host'],
+            port=mysql_db['port'],
+            database=mysql_db['database'],
+            user=mysql_db['user'],
+            password=mysql_db['password']
+        )
+        return connection
+    except Error as e:
+        print(f"Error: {e}")
+        return None
+    
--- static/js/email.js
+++ static/js/email.js
@@ -1,10 +1,10 @@
-document.getElementById('submitButton').addEventListener('click', function() {
+document.getElementById('submitButton').addEventListener('click', function () {
     // 사용자에게 구독 신청 완료 메시지를 표시합니다.
     alert("구독 신청 완료!");
-    
+
     // 구독 신청 버튼을 비활성화합니다.
     this.classList.add('disabled');
-    
+
     // 서버에 구독 요청을 보냅니다.
     fetch('/subscribe', {
         method: 'POST',
@@ -12,17 +12,17 @@ document.getElementById('submitButton').addEventListener('click', function() {
             'Content-Type': 'application/json'
         }
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            document.getElementById('submitSuccessMessage').classList.remove('d-none');
-        } else {
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                document.getElementById('submitSuccessMessage').classList.remove('d-none');
+            } else {
+                document.getElementById('submitErrorMessage').classList.remove('d-none');
+                console.log(document.getElementById('submitErrorMessage').classList.remove('d-none'));
+            }
+        })
+        .catch(error => {
             document.getElementById('submitErrorMessage').classList.remove('d-none');
-            console.log(document.getElementById('submitErrorMessage').classList.remove('d-none')); 
-        }
-    })
-    .catch(error => {
-        document.getElementById('submitErrorMessage').classList.remove('d-none'); 
-        console.log(document.getElementById('submitErrorMessage').classList.remove('d-none')); 
-    });
+            console.log(document.getElementById('submitErrorMessage').classList.remove('d-none'));
+        });
 });
